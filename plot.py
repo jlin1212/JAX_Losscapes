1b7a6539cd940e01744175cad2d385a6cf1eb278
tmillhou <tyler.millhouse@gmail.com>
2023-07-31 12:00:15 -0700

    Update plot.py
    Additional notes, documentation, and reporting features. General clean up. Accuracy landscapes.

--- plot.py
+++ plot.py
@@ -92,46 +92,56 @@ class LandscapeProblem():
                  save=False,
                  test=True,
                  sample_rate=1,
+                 test_loss_stop = 0,
+                 test_loss_strikes = 1,
                  **kwargs):
-    params_path = []
     
+    #Initialize model and param path
+    params_path = []    
     params = self.model.init(jax.random.PRNGKey(0), self.get_batch()[0]) if starting_params == None else starting_params
-    #params = flax.core.frozen_dict.unfreeze(params)
-    #initializer = jax.nn.initializers.kaiming_normal()
-    #print(params.keys())
-    #for key in params.keys():
-      #print(params[key].keys())
-      #for subkey in params[key]:
+
+    #Note: Not sure why this breaks training.
+    """ params = flax.core.frozen_dict.unfreeze(params)
+    initializer = jax.nn.initializers.kaiming_normal()
+    print(params.keys())
+    for key in params.keys():
+      print(params[key].keys())
+      for subkey in params[key]:
         #print(params[key][subkey].keys())        
-        #params[key][subkey]["kernel"] = initializer(jax.random.PRNGKey(42), params[key][subkey]["kernel"].shape, jnp.float32)  
-        #params[key][subkey]["kernel"]*=1
-    #params = flax.core.frozen_dict.freeze(params)
+        params[key][subkey]["kernel"] = initializer(jax.random.PRNGKey(42), params[key][subkey]["kernel"].shape, jnp.float32)  
+        params[key][subkey]["kernel"]*=10
+    params = flax.core.frozen_dict.freeze(params) """
 
     opt_state = optimizer.init(params)
     loss_value = -1
 
+    #Define training update step 
     @jax.jit
-    def step(params, opt_state, batch, labels):
+    def step(params, opt_state, batch, labels):      
       loss_value, grads = jax.value_and_grad(self.eval_params)(params, batch, labels)
       updates, opt_state = optimizer.update(grads, opt_state, params)
       params = optax.apply_updates(params, updates)
       return params, opt_state, loss_value
 
+    #Initialize training-related variables
     total_epochs = epochs if not single_sample else 3
-    grokometer = 0
+
+    if test_loss_stop:
+      test_loss_counter = 0 #counts times test loss surpasses [test_loss_stop]      
     best_test_loss = np.inf
     best_train_loss = np.inf
-    for epoch in range(total_epochs):
-      if epoch%50==0:
-        print('Epoch %d/%d' % (epoch, total_epochs))
-      
+    
+    #Training loop
+    print("Training...\n")
+
+    for epoch in range(total_epochs):      
+
+      #Training     
       train_loss = 0.0
       iters = self.dataset_len()//self.batch_size
-      #dataset_iter = tqdm(range(iters))
       for i in range(iters):
         batch, labels = self.get_batch()        
         params, opt_state, loss_value = step(params, opt_state, batch, labels)
-        #dataset_iter.set_description('Loss: %f' % loss_value)
         if single_sample:
           params_path.append(params)
         train_loss += loss_value
@@ -139,57 +149,55 @@ class LandscapeProblem():
       if train_loss<best_train_loss:
         best_train_loss=train_loss
 
-
       if not single_sample and epoch%sample_rate==0:
         params_path.append(params)
 
-      if epoch%50==0:
+      #Eval and reporting
+      if epoch%sample_rate==0:
         train_acc = self.accuracy(params, batch, labels)
         if test:
-          #print("Testing...")
           test_loss = 0.0
           iters = self.dataset_len(False)//self.batch_size 
-          #test_iter = tqdm(range(iters))
           for i in range(iters):
             batch, labels = self.get_batch(False)
             test_value = self.eval_params(params, batch, labels)
-            test_loss += test_value            
-            #test_iter.set_description('Loss: %f' % loss_value)
+            test_loss += test_value    
           test_loss /= iters
           test_acc = self.accuracy(params, batch, labels)
 
           if test_loss<best_test_loss:
             best_test_loss=test_loss
+
+        print('Epoch %d/%d' % (epoch, total_epochs))
         print('Train Loss: %f' % (train_loss))
         print('Train Acc.: %f' % (train_acc))
         print('Best Train Loss: %f' % (best_train_loss))
         if test:
           print('Test Loss: %f' % (test_loss))
           print('Test Acc.: %f' % (test_acc))
-          print('Best Test Loss: %f' % (best_test_loss))
-          #print(self.accuracy(params, batch, labels))
-        print("\n")
-      if test_acc>.96 and epoch%sample_rate==0:
-        grokometer+=1
-        if grokometer == 2:
-            break  
+          print('Best Test Loss: %f' % (best_test_loss))                  
+
+          if test_loss_stop:
+            if test_acc>test_loss_stop:
+              test_loss_counter+=1
+              if test_loss_counter >= test_loss_strikes:
+                  print("\n") 
+                  break     
+                        
+        print("\n") 
     return params_path
 
 class LossVisualizer():
   def flatten_path(self, parameter_path):
-    print(len(parameter_path))
+    """Flattens nested dict params to (steps, param count) shaped array. Destructive to save memory."""           
     _, unravel = jax.flatten_util.ravel_pytree(parameter_path[0])
     self.unravel = unravel
     params = jax.flatten_util.ravel_pytree(parameter_path.pop(0))[0][None,...]
     print("Flattening Parameter Path...")
     for i in tqdm(range(len(parameter_path))):
-      params = np.concatenate((params,jax.flatten_util.ravel_pytree(parameter_path.pop(0))[0][None,...]))
-      #print(i)
-      #parameter_path[i] = jax.flatten_util.ravel_pytree(parameter_path[i])[0]
-    #param_vec = [jax.flatten_util.ravel_pytree(params)[0] for params in parameter_path]
-    #param_vec = jnp.stack(param_vec)
-    #parameter_path = jnp.stack(parameter_path)
+      params = np.concatenate((params,jax.flatten_util.ravel_pytree(parameter_path.pop(0))[0][None,...]))      
     print("Done!")
+    
     return params
   
   @partial(jax.jit, static_argnums=(0,1))
@@ -197,9 +205,17 @@ class LossVisualizer():
                 landscape,
                 param_vec,
                 batch,
+                label):    
+      return jax.vmap(lambda params: landscape.eval_params(self.unravel(params), batch, label), in_axes=0, out_axes=0)(param_vec)    
+    
+  @partial(jax.jit, static_argnums=(0,1))
+  def plot_path_acc(self,
+                landscape,
+                param_vec,
+                batch,
                 label):
-    return jax.vmap(lambda params: landscape.eval_params(self.unravel(params), batch, label), in_axes=0, out_axes=0)(param_vec)
-
+      return jax.vmap(lambda params: landscape.accuracy(self.unravel(params), batch, label), in_axes=0, out_axes=0)(param_vec)
+    
   @partial(jax.jit, static_argnums=(0,1))
   def plot_surface(self, 
                    landscape, 
@@ -210,16 +226,31 @@ class LossVisualizer():
                    lowdim_path,
                    batch,
                    label):
+    
     return jax.vmap(
       lambda delta: landscape.eval_params(self.unravel(base_vec + delta[0]*x_vec + delta[1]*y_vec), batch, label), 
+      in_axes=0, out_axes=0
+    )(row_deltas)    
+  
+  @partial(jax.jit, static_argnums=(0,1))
+  def plot_surface_acc(self, 
+                   landscape, 
+                   base_vec, 
+                   x_vec,
+                   y_vec,
+                   row_deltas,
+                   lowdim_path,
+                   batch,
+                   label):                   
+    return jax.vmap(
+      lambda delta: landscape.accuracy(self.unravel(base_vec + delta[0]*x_vec + delta[1]*y_vec), batch, label), 
       in_axes=0, out_axes=0
     )(row_deltas)
 
   def project(self, param_vec):
     pca = IncrementalPCA(n_components=2)    
-    pca.fit(np.concatenate((param_vec,-param_vec)))###################
+    pca.fit(np.concatenate((param_vec,-param_vec))) #Ensures mean-centering keeps param_vec[-1,:] at origin
     self.lowdim_fit = pca
-    # print(pca.explained_variance_ratio_)
     return pca
   
   def uniform_gap_distribution(self, landscape, parameter_path, samples=100):
@@ -239,10 +270,14 @@ class LossVisualizer():
     return gaps
   
   def mill_plot(self, landscape, parameter_path, filenames, **kwargs):
+    
+    #Generate path and surface data 
     surface_data = self.process(landscape, parameter_path, **kwargs)
 
+    #Exclude PCA dims to reduce file size
     dir_x_y = "null" #[surface_data['dir_x'].astype(np.float16).tolist(),surface_data['dir_y'].astype(np.float16).tolist()],
 
+    #Generate object with training path and surface
     output_train = {
       'path': {
         'bounds': [[float(jnp.amin(surface_data['loss_x'])), float(jnp.amax(surface_data['loss_x']))],
@@ -261,6 +296,8 @@ class LossVisualizer():
         'notes': ['Minimum Sampled Loss: %f' % jnp.amin(surface_data['z_train'])]
       }
     }
+
+    #Generate object with test path and surface
     output_test = {
       'path': {
         'bounds': [[float(jnp.amin(surface_data['loss_x'])), float(jnp.amax(surface_data['loss_x']))],
@@ -280,13 +317,17 @@ class LossVisualizer():
       }
     }
 
+    #Write training data JSON file
     with open(filenames[0], 'w') as output_fp:
       json.dump(output_train, output_fp)
       print('Wrote JSON output to %s.' % filenames[0])
-    
+
+    #Write test data JSON file
     with open(filenames[1], 'w') as output_fp:
       json.dump(output_test, output_fp)
       print('Wrote JSON output to %s.' % filenames[1])
+
+    print("\n")
 
   def plotly_plot(self, landscape, parameter_path, **kwargs):
     surface_data = self.process(landscape, parameter_path, **kwargs)
@@ -360,81 +401,139 @@ class LossVisualizer():
               margin_factor=1.25,
               filter_norm=False,
               verbose=True,
-              sample_size =256):
-    param_vec = self.flatten_path(parameter_path)[len(parameter_path)//2:]
+              sample_size =256,
+              truncate_pct = 0,
+              drop_first = 1,
+              resample = 128,
+              accuracy = False):
     
-    param_vec = resamplePath(param_vec,256)
+    ###Prepare Path & Find Projection###
+    print("Generating viz data...\n")
+    
 
+    #Drop first [drop_first] steps or drop first [truncate_pct] steps (whichever is greater)    
+    if verbose: print("Initial Path Length: " + str(len(parameter_path)))    
+    assert truncate_pct>=0 and truncate_pct<=(len(parameter_path)-1/(len(parameter_path)))
+    assert drop_first<len(parameter_path)
+    param_start = max(drop_first,int(np.floor(len(parameter_path)*truncate_pct)))    
+    parameter_path = parameter_path[param_start:]
+
+    #Flatten params from nested dict to array of shape (steps, parameter count)
+    param_vec = self.flatten_path(parameter_path)   
+
+    #Get [resample] evenly-spaced steps along path (preserves first and last steps) 
+    assert resample>=3
+    if resample:
+      print("Truncated Path Length: " + str(param_vec.shape[0]))       
+      param_vec = resamplePath(param_vec,resample) 
+      print("Resampled Path Length: " + str(param_vec.shape[0])+"\n") 
+    else: 
+      print("Truncated Path Length: " + str(param_vec.shape[0])+"\n") 
+
+    #Get final (included) param step 
     self.base_vec = param_vec[-1,:][None,...]
+    
+    #Center on final step
     param_delta = param_vec - self.base_vec
-    #print(param_delta[-1])
-
+    
+    #Get PCA projection
     self.project(param_delta)
 
+    #Apply filter norm
     if filter_norm:
       for i in range(2):
         self.lowdim_fit.components_[i] = self.filter_norm(self.lowdim_fit.components_[i], parameter_path[-1])
 
+    #Get projected path
     lowdim_path = self.lowdim_fit.transform(param_delta)
 
-    
-
+    #Compute plot bounds
     amp = margin_factor * np.amax(np.abs(lowdim_path-lowdim_path[-1,:])) if x_range == None else x_range
-    #y_amp = margin_factor * np.amax(np.abs(lowdim_path[:,1])) if y_range == None else y_range
-    # x_amp = y_amp = margin_factor
-
-    #print(lowdim_path[-1,:])
-    #print(lowdim_path[0,:])
-
-    x_mesh, y_mesh = jnp.meshgrid(jnp.linspace(lowdim_path[-1,0]-amp, lowdim_path[-1,0]+amp, resolution), jnp.linspace(lowdim_path[-1,1]-amp, lowdim_path[-1,1]+amp, resolution))
-
-    deltas = jnp.stack([x_mesh, y_mesh], axis=2)
-    #print(deltas[50,50,:])
-    if verbose: print(deltas.shape)
     
-    """ iters = landscape.dataset_len(False)//landscape.batch_size
-    param_losses = np.zeros(param_vec.shape[0])
-    for i in range(iters):
-      batch, label = landscape.get_batch(training)
-      param_losses += self.plot_path(landscape, param_vec, batch, label)
-    param_losses/=iters
-
-    """
-
-    #Plot paths and surfaces
-
+    #Compute grid coordinates
+    x_mesh, y_mesh = jnp.meshgrid(jnp.linspace(lowdim_path[-1,0]-amp, lowdim_path[-1,0]+amp, resolution), jnp.linspace(lowdim_path[-1,1]-amp, lowdim_path[-1,1]+amp, resolution))
+    deltas = jnp.stack([x_mesh, y_mesh], axis=2)
+        
+    #Set projection dims    
     dir_x = self.lowdim_fit.components_[0] if x_vec is None else x_vec
     dir_y = self.lowdim_fit.components_[1] if y_vec is None else y_vec
+    
+    
+    ###Build Viz Data###
+
+    #Set sample size for plot evals
     landscape.batch_size = sample_size
     
-    #Training
-    landscape.pivot_train= np.inf
+    #Get training data
+    landscape.pivot_train = np.inf #Forces data reshuffle
     batch, label = landscape.get_batch(training=True)
-    param_losses_train = self.plot_path(landscape, param_vec, batch, label)
-    print(param_losses_train[-1])
+
+    #Plot training path
+    if accuracy: #z vals = accuracy
+      param_losses_train = self.plot_path_acc(landscape, param_vec, batch, label)
+    else: #z vals = loss
+      param_losses_train = self.plot_path(landscape, param_vec, batch, label)    
+
+    if verbose: print("First/Last Training Step Losses: " + str((float(param_losses_train[0]), float(param_losses_train[-1]))))
     
-    Z_train = jax.lax.map(lambda row_deltas:
-      self.plot_surface(
-          landscape,
-          param_vec[-1,:], 
-          dir_x,
-          dir_y,
-          row_deltas,
-          lowdim_path,
-          batch,
-          label
-      ), 
-    deltas)
+    #Plot training surface
+    if accuracy: #z vals = accuracy
+      Z_train = jax.lax.map(lambda row_deltas:
+        self.plot_surface_acc(
+            landscape,
+            param_vec[-1,:], 
+            dir_x,
+            dir_y,
+            row_deltas,
+            lowdim_path,
+            batch,
+            label
+        ), 
+      deltas)
+    else: #z vals = loss
+      Z_train = jax.lax.map(lambda row_deltas:
+        self.plot_surface(
+            landscape,
+            param_vec[-1,:], 
+            dir_x,
+            dir_y,
+            row_deltas,
+            lowdim_path,
+            batch,
+            label
+        ), 
+      deltas)
 
-    if verbose: print(Z_train.shape)
+    if verbose: print("Training Surface Data Shape: " +str(Z_train.shape))
 
-    #Test
-    landscape.pivot_test = np.inf
+    #Get test data
+    landscape.pivot_test = np.inf #Forces data reshuffle
     batch, label = landscape.get_batch(training=False)
-    param_losses_test = self.plot_path(landscape, param_vec, batch, label)
-    print(param_losses_test[-1])
+
+    #Plot test path
+    if accuracy:
+      param_losses_test = self.plot_path_acc(landscape, param_vec, batch, label)
+    else:
+      param_losses_test = self.plot_path(landscape, param_vec, batch, label)
+
+    if verbose: print("First/Last Test Step Losses: " + str((float(param_losses_test[0]), float(param_losses_test[-1]))))
     
-    Z_test = jax.lax.map(lambda row_deltas:
+    #Plot test surface
+    if accuracy: #z vals = accuracy
+      Z_test = jax.lax.map(lambda row_deltas:
+        self.plot_surface_acc(
+            landscape,
+            param_vec[-1,:], 
+            dir_x,
+            dir_y,
+            row_deltas,
+            lowdim_path,
+            batch,
+            label
+        ), 
+      deltas)
+    else: #z vals = loss
+      Z_test = jax.lax.map(lambda row_deltas:
       self.plot_surface(
           landscape,
           param_vec[-1,:], 
@@ -446,12 +545,10 @@ class LossVisualizer():
           label
       ), 
     deltas)
+    
+    if verbose: print("Test Surface Data Shape: " +str(Z_test.shape) +"\n")
 
-    # Z = jnp.sin(5 * x_mesh) + jnp.sin(5 * y_mesh)
-
-    #print(Z[50,50])
-    if verbose: print(Z_test.shape)
-
+    #Return data for conversion to JSON
     return {
         'x': x_mesh,
         'y': y_mesh,
